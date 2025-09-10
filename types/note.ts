fe776d104dae042f32c2b49d9940f19f5406a9cd
Ihor Tonkovyd <ihor.tonkovyd@gmail.com>
2025-09-10 22:53:57 +0300

    Update: fixing typization

--- types/note.ts
+++ types/note.ts
@@ -4,10 +4,10 @@ export type BaseNoteParams = {
   title: string;
   content: string;
   tag: noteTag;
-  createdAt: string;
-  updatedAt: string;
 };
 
 export type Note = BaseNoteParams & {
   id: string;
+  createdAt?: string;
+  updatedAt?: string;
 };
