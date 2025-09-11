251af4b76abe60caa6351a07f4b8178d663c77f2
Ihor Tonkovyd <ihor.tonkovyd@gmail.com>
2025-09-11 21:54:47 +0300

    Update: removing <any> tupes from useNoteMutation hook

--- hooks/useNoteMutation.ts
+++ hooks/useNoteMutation.ts
@@ -1,15 +1,15 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { toast } from 'react-hot-toast';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
 
 interface UseNoteMutationOptions<T> {
-  mutationFn: (variables: T) => Promise<any>;
+  mutationFn: (variables: T) => Promise<T>;
   queryKey: string[];
   successMsg?: string;
   errorMsg?: string;
   successAction?: () => void;
 }
 
-export function useNoteMutation<T = any>({
+export function useNoteMutation<T>({
   mutationFn,
   queryKey,
   successMsg,
