4ccc003601e06b47c0b68628fad13d0ad082f66d
Ihor Tonkovyd <ihor.tonkovyd@gmail.com>
2025-09-11 22:02:45 +0300

    Update: adding prefetch and hydration to Notes page

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,10 +1,26 @@
+import {
+  HydrationBoundary,
+  dehydrate,
+  QueryClient,
+} from "@tanstack/react-query";
+
 import { fetchAllNotes } from "@/lib/api";
+
 import NotesClient from "./Notes.client";
 
 const Notes = async () => {
-  await fetchAllNotes(1);
+  const queryClient = new QueryClient();
 
-  return <NotesClient />;
+  await queryClient.prefetchQuery({
+    queryKey: ["notes"],
+    queryFn: () => fetchAllNotes(1),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
+  );
 };
 
 export default Notes;
